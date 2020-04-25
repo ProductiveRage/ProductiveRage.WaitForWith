a805587bdcceff0d600069373b68964b5023abca
Dan Roberts <dangger36@gmail.com>
2020-04-25 20:41:04 +0100

    First code commit

---
+++ Generator/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Generator
+{
+    // TODO: Note in docs lack of "Async" suffix(?) - https://docs.particular.net/nservicebus/upgrades/5to6/async-suffix
+    internal static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string outputFilename = "ProductiveRageTaskExtensions.cs";
+
+            var outputFolderName = args?.FirstOrDefault(arg => !string.IsNullOrWhiteSpace(arg));
+            if (outputFolderName is null)
+            {
+                Console.WriteLine($"Must specify the output folder to write {outputFilename} to as a command line argument");
+                Environment.Exit(1);
+                return;
+            }
+            var outputFolder = new DirectoryInfo(outputFolderName);
+            if (!outputFolder.Exists)
+            {
+                Console.WriteLine($"The folder to write {outputFilename} to must already exist: " + outputFolderName);
+                Environment.Exit(1);
+                return;
+            }
+
+            var templateContent = await File.ReadAllLinesAsync("ProductiveRageTaskExtensions.template");
+            var firstLineWithIndentation = templateContent.First(line => (line.Length != 0) && char.IsWhiteSpace(line[0]));
+
+            var completedContent = string.Join(
+                Environment.NewLine,
+                templateContent.SelectMany(line =>
+                    line.Trim() == "# Start Here"
+                        ? GenerateMethods(initialIndentation: GetIndentationFromLine(line), individualIndentation: GetIndentationFromLine(firstLineWithIndentation))
+                        : new[] { line }
+                )
+            );
+
+            var outputFilePath = Path.Combine(outputFolderName, outputFilename);
+            File.WriteAllText(outputFilePath, completedContent);
+
+            static string GetIndentationFromLine(string line) => new string(line.TakeWhile(char.IsWhiteSpace).ToArray());
+        }
+
+        private static IEnumerable<string> GenerateMethods(string initialIndentation, string individualIndentation)
+        {
+            var haveWrittenAnything = false;
+            for (var numberOfOtherTasks = 1; numberOfOtherTasks <= 9; numberOfOtherTasks++)
+            {
+                if (haveWrittenAnything)
+                    yield return "";
+                var lines = GenerateMethods(initialIndentation, individualIndentation, numberOfOtherTasks);
+                foreach (var line in lines)
+                    yield return line;
+                haveWrittenAnything = true;
+            }
+        }
+
+        private static IEnumerable<string> GenerateMethods(string initialIndentation, string individualIndentation, int numberOfOtherTasks)
+        {
+            var typeParams = string.Join(", ", Enumerable.Range(1, 1 + numberOfOtherTasks).Select(i => "T" + i));
+            var othersParameterTypeNames = Enumerable.Range(1, numberOfOtherTasks).Select(i => $"Task<T{i + 1}>");
+            var othersParameterNames = (numberOfOtherTasks == 1)
+                ? new[] { "other" }
+                : Enumerable.Range(1, numberOfOtherTasks).Select(i => $"other{i}");
+
+            var allTaskNames = othersParameterNames.Prepend("source");
+            var coreCombinedTask = "Task.WhenAll(" + string.Join(", ", allTaskNames) + ")";
+            var returnAllResults = "return (" + string.Join(", ", allTaskNames.Select(name => name + ".Result")) + ");";
+
+            // Method with CancellationToken option (will has a default value if not required - can't have this default AND the timeout one with a default otherwise the compiler would struggle with ambiguous overload matching)
+            var cancellationTokenName = "cancellationToken";
+            foreach (var line in GenerateMethodHeader("CancellationToken " + cancellationTokenName + " = default"))
+                yield return line;
+            yield return "";
+            yield return initialIndentation + individualIndentation + "await WaitWithPossibilityOfCancellation(" + coreCombinedTask + ", " + cancellationTokenName + ").ConfigureAwait(false);";
+            yield return initialIndentation + individualIndentation + returnAllResults;
+            yield return initialIndentation + "}";
+            yield return "";
+
+            // Method with timeout option
+            var timeoutParameterName = "timeout";
+            foreach (var line in GenerateMethodHeader("TimeSpan " + timeoutParameterName))
+                yield return line;
+            yield return initialIndentation + individualIndentation + "ThrowForNegativeTimeout(" + timeoutParameterName + ", nameof(" + timeoutParameterName + "));";
+            yield return "";
+            yield return initialIndentation + individualIndentation + "await WaitWithPossibilityOfCancellation(" + coreCombinedTask + ", Task.Delay(" + timeoutParameterName + ")).ConfigureAwait(false);";
+            yield return initialIndentation + individualIndentation + returnAllResults;
+            yield return initialIndentation + "}";
+
+            IEnumerable<string> GenerateMethodHeader(string optionalAdditionalArguments = null)
+            {
+                yield return string.Format(
+                    "{0}public static async Task<({1})> WaitForWith<{1}>(this Task<T1> source, {2}{3})",
+                    initialIndentation,
+                    typeParams,
+                    string.Join(", ", othersParameterTypeNames.Zip(othersParameterNames, (typeName, name) => typeName + " " + name)),
+                    string.IsNullOrWhiteSpace(optionalAdditionalArguments) ? "" : (", " + optionalAdditionalArguments)
+                );
+                yield return initialIndentation + "{";
+                foreach (var name in allTaskNames)
+                    yield return initialIndentation + individualIndentation + "ThrowForNullTask(" + name + ", nameof(" + name + "));";
+            }
+        }
+    }
+}
---
+++ WaitForWith/ProductiveRageTaskExtensions.cs
@@ -0,0 +1,298 @@
+using System.Diagnostics;
+
+namespace System.Threading.Tasks
+{
+    public static class ProductiveRageTaskExtensions
+    {
+        public static async Task<(T1, T2)> WaitForWith<T1, T2>(this Task<T1> source, Task<T2> other, CancellationToken cancellationToken = default)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other, nameof(other));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other), cancellationToken).ConfigureAwait(false);
+            return (source.Result, other.Result);
+        }
+
+        public static async Task<(T1, T2)> WaitForWith<T1, T2>(this Task<T1> source, Task<T2> other, TimeSpan timeout)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other, nameof(other));
+            ThrowForNegativeTimeout(timeout, nameof(timeout));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other), Task.Delay(timeout)).ConfigureAwait(false);
+            return (source.Result, other.Result);
+        }
+
+        public static async Task<(T1, T2, T3)> WaitForWith<T1, T2, T3>(this Task<T1> source, Task<T2> other1, Task<T3> other2, CancellationToken cancellationToken = default)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2), cancellationToken).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result);
+        }
+
+        public static async Task<(T1, T2, T3)> WaitForWith<T1, T2, T3>(this Task<T1> source, Task<T2> other1, Task<T3> other2, TimeSpan timeout)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+            ThrowForNegativeTimeout(timeout, nameof(timeout));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2), Task.Delay(timeout)).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4)> WaitForWith<T1, T2, T3, T4>(this Task<T1> source, Task<T2> other1, Task<T3> other2, Task<T4> other3, CancellationToken cancellationToken = default)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+            ThrowForNullTask(other3, nameof(other3));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2, other3), cancellationToken).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result, other3.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4)> WaitForWith<T1, T2, T3, T4>(this Task<T1> source, Task<T2> other1, Task<T3> other2, Task<T4> other3, TimeSpan timeout)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+            ThrowForNullTask(other3, nameof(other3));
+            ThrowForNegativeTimeout(timeout, nameof(timeout));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2, other3), Task.Delay(timeout)).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result, other3.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5)> WaitForWith<T1, T2, T3, T4, T5>(this Task<T1> source, Task<T2> other1, Task<T3> other2, Task<T4> other3, Task<T5> other4, CancellationToken cancellationToken = default)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+            ThrowForNullTask(other3, nameof(other3));
+            ThrowForNullTask(other4, nameof(other4));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2, other3, other4), cancellationToken).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result, other3.Result, other4.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5)> WaitForWith<T1, T2, T3, T4, T5>(this Task<T1> source, Task<T2> other1, Task<T3> other2, Task<T4> other3, Task<T5> other4, TimeSpan timeout)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+            ThrowForNullTask(other3, nameof(other3));
+            ThrowForNullTask(other4, nameof(other4));
+            ThrowForNegativeTimeout(timeout, nameof(timeout));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2, other3, other4), Task.Delay(timeout)).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result, other3.Result, other4.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6)> WaitForWith<T1, T2, T3, T4, T5, T6>(this Task<T1> source, Task<T2> other1, Task<T3> other2, Task<T4> other3, Task<T5> other4, Task<T6> other5, CancellationToken cancellationToken = default)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+            ThrowForNullTask(other3, nameof(other3));
+            ThrowForNullTask(other4, nameof(other4));
+            ThrowForNullTask(other5, nameof(other5));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2, other3, other4, other5), cancellationToken).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result, other3.Result, other4.Result, other5.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6)> WaitForWith<T1, T2, T3, T4, T5, T6>(this Task<T1> source, Task<T2> other1, Task<T3> other2, Task<T4> other3, Task<T5> other4, Task<T6> other5, TimeSpan timeout)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+            ThrowForNullTask(other3, nameof(other3));
+            ThrowForNullTask(other4, nameof(other4));
+            ThrowForNullTask(other5, nameof(other5));
+            ThrowForNegativeTimeout(timeout, nameof(timeout));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2, other3, other4, other5), Task.Delay(timeout)).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result, other3.Result, other4.Result, other5.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6, T7)> WaitForWith<T1, T2, T3, T4, T5, T6, T7>(this Task<T1> source, Task<T2> other1, Task<T3> other2, Task<T4> other3, Task<T5> other4, Task<T6> other5, Task<T7> other6, CancellationToken cancellationToken = default)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+            ThrowForNullTask(other3, nameof(other3));
+            ThrowForNullTask(other4, nameof(other4));
+            ThrowForNullTask(other5, nameof(other5));
+            ThrowForNullTask(other6, nameof(other6));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2, other3, other4, other5, other6), cancellationToken).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result, other3.Result, other4.Result, other5.Result, other6.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6, T7)> WaitForWith<T1, T2, T3, T4, T5, T6, T7>(this Task<T1> source, Task<T2> other1, Task<T3> other2, Task<T4> other3, Task<T5> other4, Task<T6> other5, Task<T7> other6, TimeSpan timeout)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+            ThrowForNullTask(other3, nameof(other3));
+            ThrowForNullTask(other4, nameof(other4));
+            ThrowForNullTask(other5, nameof(other5));
+            ThrowForNullTask(other6, nameof(other6));
+            ThrowForNegativeTimeout(timeout, nameof(timeout));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2, other3, other4, other5, other6), Task.Delay(timeout)).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result, other3.Result, other4.Result, other5.Result, other6.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8)> WaitForWith<T1, T2, T3, T4, T5, T6, T7, T8>(this Task<T1> source, Task<T2> other1, Task<T3> other2, Task<T4> other3, Task<T5> other4, Task<T6> other5, Task<T7> other6, Task<T8> other7, CancellationToken cancellationToken = default)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+            ThrowForNullTask(other3, nameof(other3));
+            ThrowForNullTask(other4, nameof(other4));
+            ThrowForNullTask(other5, nameof(other5));
+            ThrowForNullTask(other6, nameof(other6));
+            ThrowForNullTask(other7, nameof(other7));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2, other3, other4, other5, other6, other7), cancellationToken).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result, other3.Result, other4.Result, other5.Result, other6.Result, other7.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8)> WaitForWith<T1, T2, T3, T4, T5, T6, T7, T8>(this Task<T1> source, Task<T2> other1, Task<T3> other2, Task<T4> other3, Task<T5> other4, Task<T6> other5, Task<T7> other6, Task<T8> other7, TimeSpan timeout)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+            ThrowForNullTask(other3, nameof(other3));
+            ThrowForNullTask(other4, nameof(other4));
+            ThrowForNullTask(other5, nameof(other5));
+            ThrowForNullTask(other6, nameof(other6));
+            ThrowForNullTask(other7, nameof(other7));
+            ThrowForNegativeTimeout(timeout, nameof(timeout));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2, other3, other4, other5, other6, other7), Task.Delay(timeout)).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result, other3.Result, other4.Result, other5.Result, other6.Result, other7.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8, T9)> WaitForWith<T1, T2, T3, T4, T5, T6, T7, T8, T9>(this Task<T1> source, Task<T2> other1, Task<T3> other2, Task<T4> other3, Task<T5> other4, Task<T6> other5, Task<T7> other6, Task<T8> other7, Task<T9> other8, CancellationToken cancellationToken = default)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+            ThrowForNullTask(other3, nameof(other3));
+            ThrowForNullTask(other4, nameof(other4));
+            ThrowForNullTask(other5, nameof(other5));
+            ThrowForNullTask(other6, nameof(other6));
+            ThrowForNullTask(other7, nameof(other7));
+            ThrowForNullTask(other8, nameof(other8));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2, other3, other4, other5, other6, other7, other8), cancellationToken).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result, other3.Result, other4.Result, other5.Result, other6.Result, other7.Result, other8.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8, T9)> WaitForWith<T1, T2, T3, T4, T5, T6, T7, T8, T9>(this Task<T1> source, Task<T2> other1, Task<T3> other2, Task<T4> other3, Task<T5> other4, Task<T6> other5, Task<T7> other6, Task<T8> other7, Task<T9> other8, TimeSpan timeout)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+            ThrowForNullTask(other3, nameof(other3));
+            ThrowForNullTask(other4, nameof(other4));
+            ThrowForNullTask(other5, nameof(other5));
+            ThrowForNullTask(other6, nameof(other6));
+            ThrowForNullTask(other7, nameof(other7));
+            ThrowForNullTask(other8, nameof(other8));
+            ThrowForNegativeTimeout(timeout, nameof(timeout));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2, other3, other4, other5, other6, other7, other8), Task.Delay(timeout)).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result, other3.Result, other4.Result, other5.Result, other6.Result, other7.Result, other8.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10)> WaitForWith<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(this Task<T1> source, Task<T2> other1, Task<T3> other2, Task<T4> other3, Task<T5> other4, Task<T6> other5, Task<T7> other6, Task<T8> other7, Task<T9> other8, Task<T10> other9, CancellationToken cancellationToken = default)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+            ThrowForNullTask(other3, nameof(other3));
+            ThrowForNullTask(other4, nameof(other4));
+            ThrowForNullTask(other5, nameof(other5));
+            ThrowForNullTask(other6, nameof(other6));
+            ThrowForNullTask(other7, nameof(other7));
+            ThrowForNullTask(other8, nameof(other8));
+            ThrowForNullTask(other9, nameof(other9));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2, other3, other4, other5, other6, other7, other8, other9), cancellationToken).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result, other3.Result, other4.Result, other5.Result, other6.Result, other7.Result, other8.Result, other9.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10)> WaitForWith<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(this Task<T1> source, Task<T2> other1, Task<T3> other2, Task<T4> other3, Task<T5> other4, Task<T6> other5, Task<T7> other6, Task<T8> other7, Task<T9> other8, Task<T10> other9, TimeSpan timeout)
+        {
+            ThrowForNullTask(source, nameof(source));
+            ThrowForNullTask(other1, nameof(other1));
+            ThrowForNullTask(other2, nameof(other2));
+            ThrowForNullTask(other3, nameof(other3));
+            ThrowForNullTask(other4, nameof(other4));
+            ThrowForNullTask(other5, nameof(other5));
+            ThrowForNullTask(other6, nameof(other6));
+            ThrowForNullTask(other7, nameof(other7));
+            ThrowForNullTask(other8, nameof(other8));
+            ThrowForNullTask(other9, nameof(other9));
+            ThrowForNegativeTimeout(timeout, nameof(timeout));
+
+            await WaitWithPossibilityOfCancellation(Task.WhenAll(source, other1, other2, other3, other4, other5, other6, other7, other8, other9), Task.Delay(timeout)).ConfigureAwait(false);
+            return (source.Result, other1.Result, other2.Result, other3.Result, other4.Result, other5.Result, other6.Result, other7.Result, other8.Result, other9.Result);
+        }
+
+        private static Task WaitWithPossibilityOfCancellation(Task work, CancellationToken cancellationToken)
+        {
+            ThrowForNullTask(work, nameof(work));
+
+            return cancellationToken.CanBeCanceled
+                ? WaitWithPossibilityOfCancellation(work, cancellationToken.AsTask())
+                : work;
+        }
+
+        private static async Task WaitWithPossibilityOfCancellation(Task work, Task cancellerIfAny)
+        {
+            ThrowForNullTask(work, nameof(work));
+
+            if (cancellerIfAny is null)
+            {
+                await work.ConfigureAwait(false);
+                return;
+            }
+
+            var taskThatWon = await Task.WhenAny(work, cancellerIfAny).ConfigureAwait(false);
+            if (taskThatWon == cancellerIfAny)
+                throw new OperationCanceledException();
+        }
+
+        private static Task AsTask(this CancellationToken cancellationToken) // Courtesy of https://github.com/StephenCleary/AsyncEx
+        {
+            var tcs = new TaskCompletionSource<object>();
+            cancellationToken.Register(
+                () => tcs.TrySetCanceled(),
+                useSynchronizationContext: false
+            );
+            return tcs.Task;
+        }
+
+        [DebuggerStepThrough]
+        private static void ThrowForNullTask(Task value, string parameterName)
+        {
+            if (value is null)
+                throw new ArgumentNullException(parameterName);
+        }
+
+        [DebuggerStepThrough]
+        private static void ThrowForNegativeTimeout(TimeSpan value, string timeoutParameterName)
+        {
+            if (value.Ticks < 0)
+                throw new ArgumentOutOfRangeException(timeoutParameterName, "must be zero / default(meaning do-not-timeout) or a positive duration");
+        }
+    }
+}
